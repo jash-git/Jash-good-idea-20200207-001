5ba5394e466821197222200d1e303765f50893e6
Gitea <gitea@fake.local>
2020-02-11 10:19:20 +0800

    Create

---
+++ 武漢肺炎梗圖/捐百萬補償金 陽明校長發祭品文 盼口罩留給需要的人 _ 文教新訊 _ 文教 _ 聯合新聞網_files/index.php
@@ -0,0 +1,9 @@
+var truvid_cb_7776234663010588_loaded = false;
+(function() {
+var doc = document;
+var win = window;
+loadJSFile=function(e,n,l){if(!1!==e){var a=n.createElement("script");a.src=e,a.async=!0,a.onload=function(){a.onload=null,l()},n.getElementsByTagName("body")[0].appendChild(a)}else l&&l()};
+loadJSFile('https://s.trvdp.com/scripts/v5.522/instream.js',doc,function() {
+if(truvid_cb_7776234663010588_loaded == false) {truvid_cb_7776234663010588_loaded = true;win.brWidgetInit.init('382', '2676', '7776234663010588', 'https://stg.truvidplayer.com', {"playlist_id":"1578","video_id":false}, false , 'v5.522','');}
+});
+})();
